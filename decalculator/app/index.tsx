2cdb2e1bfbe8ec9466ead9743df1185a893fcf58
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-09-25 19:01:10 +0100

    Add ThemeContext and basic theme switch

--- decalculator/app/index.tsx
+++ decalculator/app/index.tsx
@@ -1,16 +1,25 @@
-import { Text, View } from "react-native";
+import { myColors } from "@/components/styles/Colors";
+import { useState } from "react";
+import { StyleSheet, Text, View, Switch } from "react-native";
 
 export default function Index() {
+  const [theme, useTheme] = useState("light");
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={theme === "light" ? styles.container : [styles.container, {backgroundColor: myColors.dark}]}
     >
+      <Switch value={theme === "light"} onValueChange={() => useTheme(theme === "light" ? "dark" : "light")}></Switch>
       <Text>Pantalla</Text>
       <Text>Botones</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: myColors.white
+  }
+})
