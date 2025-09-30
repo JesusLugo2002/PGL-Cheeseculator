8ae1d7f84e873da7cdd546216e2ab7b48dc74731
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-09-30 16:21:23 +0100

    Progress with keyboard object

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,4 +1,7 @@
+import { styles } from "@/styles/GlobalStyles";
 import { useState } from "react";
+import { View } from "react-native";
+import Button from "./Button";
 
 export default function Keyboard() {
     const [highlightNumber, setHighlightNumber] = useState("");
@@ -10,6 +13,18 @@ export default function Keyboard() {
         setHighlightNumber("");
         setOtherNumber("");
         setResult(null);
+    }
+
+    const handleNumberPress = (buttonValue: string) => {
+        if (highlightNumber.length < 10) {
+            setHighlightNumber(highlightNumber + buttonValue);
+        }
+    }
+
+    const handleOperationPress = (buttonValue: string) => {
+        setOperation(buttonValue);
+        setOtherNumber(highlightNumber);
+        setHighlightNumber("");
     }
 
    const getResult = () => {
@@ -25,4 +40,39 @@ export default function Keyboard() {
             break;
     }
    }
+
+   return (
+    <>
+    <View style={styles.row}>
+        <Button title="C" onPress={clear}/>
+        <Button title="+/-" onPress={() => handleOperationPress("+/-")}/>
+        <Button title="%" onPress={() => handleOperationPress("%")}/>
+        <Button title="÷" onPress={() => handleOperationPress("÷")}/>
+    </View>
+    <View style={styles.row}>
+        <Button title="7" onPress={() => handleNumberPress("7")}/>
+        <Button title="8" onPress={() => handleNumberPress("8")}/>
+        <Button title="9" onPress={() => handleNumberPress("9")}/>
+        <Button title="X" onPress={() => handleOperationPress("X")}/>
+    </View>
+    <View style={styles.row}>
+        <Button title="4" onPress={() => handleNumberPress("4")}/>
+        <Button title="5" onPress={() => handleNumberPress("5")}/>
+        <Button title="6" onPress={() => handleNumberPress("6")}/>
+        <Button title="-" onPress={() => handleOperationPress("-")}/>
+    </View>
+    <View style={styles.row}>
+        <Button title="1" onPress={() => handleNumberPress("1")}/>
+        <Button title="2" onPress={() => handleNumberPress("2")}/>
+        <Button title="3" onPress={() => handleNumberPress("3")}/>
+        <Button title="+" onPress={() => handleOperationPress("+")}/>
+    </View>
+    <View style={styles.row}>
+        <Button title="." onPress={() => handleNumberPress("1")}/>
+        <Button title="0" onPress={() => handleNumberPress("2")}/>
+        <Button title="⌫" onPress={() => setHighlightNumber(highlightNumber.slice(0, -1))}/>
+        <Button title="=" onPress={() => getResult()}/>
+    </View>
+    </>
+   )
 }
