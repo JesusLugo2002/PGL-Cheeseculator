be8d9a28fd3464f1936d3cd26e24622507e12981
JesusLugo2002 <150654444+JesusLugo2002@users.noreply.github.com>
2025-09-30 15:23:49 +0100

    Crear Keyboard.tsx

---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,14 @@
+export default function Keyboard() {
+    const [highlightNumber, setHighlightNumber] = useState("");
+    const [otherNumber, setOtherNumber] = useState("");
+    const [operation, setOperation] = useState("");
+    const [result, setResult] = useState<null|number>(null);
+
+    const handleHighlightNumber = () => {
+
+    }
+
+    const handleOperation = () => {
+
+    }
+}
